f4f82a7ff0fbbdd59adbbc04c90b960d9f295a99
postar <mpocztaruk_92@hotmail.com>
2018-02-03 07:43:33 -0300

    Menu Pausa
    Agregado Menu IN Game

---
+++ Assets/Salon juego/Scripts/MenuOnEscape.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuOnEscape : MonoBehaviour
+{
+
+    public GameObject PausePanel;
+    public GameObject HelpPanel;
+    bool Paused = false;
+
+    void Start()
+    {
+     PausePanel.gameObject.SetActive(false);
+     HelpPanel.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            if (Paused == true)
+            {
+                PausePanel.gameObject.SetActive(false);
+                HelpPanel.gameObject.SetActive(false);
+                Paused = false;
+            }
+            else
+            {
+                PausePanel.gameObject.SetActive(true);
+                Paused = true;
+            }
+        }
+    }
+    public void Resume()
+    {
+        PausePanel.gameObject.SetActive(false);
+        HelpPanel.gameObject.SetActive(false);
+        Paused = false;
+    }
+}
